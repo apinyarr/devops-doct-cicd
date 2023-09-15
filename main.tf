91ee56ea2104f7bc53a1d6010f2f7e0c370d6df6
Apinya <apinyarr@hotmail.com>
2023-09-15 10:08:47 -0400

    debug set-output error

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 # The configuration for the `remote` backend.
 terraform {
     backend "remote" {
-    # The name of your Terraform Cloud organization.
-    organization = "doct-lab"
+        # The name of your Terraform Cloud organization.
+        organization = "doct-lab"
 
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-        name = "doct-lab-ws"
-    }
+        # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+            name = "doct-lab-ws"
+        }
     }
 }
 
 # An example resource that does nothing.
 resource "null_resource" "example" {
     triggers = {
-    value = "A example resource that does nothing!"
+        value = "A example resource that does nothing!"
     }
 }
